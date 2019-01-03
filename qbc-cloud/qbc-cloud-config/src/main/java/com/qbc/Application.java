03bf08a512cd02264873019b1007755d337970b1
hpu09 <hpu09@192.168.100.172>
2019-01-03 13:18:23 +0900

    拆分注册中心和配置中心

--- qbc-cloud/qbc-cloud-config/src/main/java/com/qbc/Application.java
+++ qbc-cloud/qbc-cloud-config/src/main/java/com/qbc/Application.java
@@ -2,9 +2,9 @@ package com.qbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
 public class Application {
 

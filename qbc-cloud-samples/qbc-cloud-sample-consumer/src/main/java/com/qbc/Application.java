713f438110c41709622398c541caf55aec2e4570
Ma <Ma@192.168.11.7>
2019-01-06 21:39:44 +0900

    重构代码

--- qbc-cloud-samples/qbc-cloud-sample-consumer/src/main/java/com/qbc/Application.java
+++ qbc-cloud-samples/qbc-cloud-sample-consumer/src/main/java/com/qbc/Application.java
@@ -1,13 +1,13 @@
 package com.qbc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.slyak.spring.jpa.GenericJpaRepositoryFactoryBean;
 import com.slyak.spring.jpa.GenericJpaRepositoryImpl;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableJpaRepositories(basePackages = "com.qbc", repositoryBaseClass = GenericJpaRepositoryImpl.class, repositoryFactoryBeanClass = GenericJpaRepositoryFactoryBean.class)
 public class Application {
 

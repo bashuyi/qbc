794839838d1b18fd3658770d935219acc4b8b1b3
Ma <Ma@192.168.11.7>
2019-01-06 12:50:38 +0900

    重构代码

--- qbc-cloud/qbc-cloud-registry/src/main/java/com/qbc/Application.java
+++ qbc-cloud/qbc-cloud-registry/src/main/java/com/qbc/Application.java
@@ -2,11 +2,9 @@ package com.qbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@EnableConfigServer
 @SpringBootApplication
 public class Application {
 

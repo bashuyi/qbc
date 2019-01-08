5ae2608e6260504876c23845de123499c46f47c1
hpu09 <hpu09@192.168.100.172>
2019-01-08 20:08:10 +0900

    完善认证服务

--- qbc-cloud/qbc-cloud-auth/src/main/java/com/qbc/service/TokenService.java
+++ qbc-cloud/qbc-cloud-auth/src/main/java/com/qbc/service/TokenService.java
@@ -2,11 +2,15 @@ package com.qbc.service;
 
 import javax.validation.constraints.NotEmpty;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 
+import com.qbc.dao.SysUserDAO;
+import com.qbc.dao.SysUserDO;
 import com.qbc.exception.UnauthorizedException;
 import com.qbc.manager.core.TokenManager;
+import com.qbc.manager.core.ValidationManager;
 import com.qbc.openinterface.OpenInterface;
 import com.qbc.openinterface.OpenInterfaceMethod;
 import com.qbc.openinterface.OpenInterfaceResponse;
@@ -18,12 +22,22 @@ public class TokenService {
 	@Autowired
 	private TokenManager tokenManager;
 
+	@Autowired
+	private ValidationManager validationManager;
+
+	@Autowired
+	private SysUserDAO sysUserDAO;
+
 	@OpenInterfaceMethod
 	public OpenInterfaceResponse<String> createToken(@NotEmpty String username, @NotEmpty String password) {
-		// TODO 签名密钥，根据用户名从数据库获得
-		String secret = "";
+		SysUserDO sysUserDO = sysUserDAO.findByUsername(username);
+		
+		// 认证
+		validationManager.notNull(sysUserDO);
+		validationManager.assertTrue(StringUtils.equals(password, sysUserDO.getPassword()));
+
 		// 生成Token
-		String token = tokenManager.createToken(username, secret);
+		String token = tokenManager.createToken(username, sysUserDO.getSecret());
 
 		return OpenInterfaceResponse.ok(token);
 	}

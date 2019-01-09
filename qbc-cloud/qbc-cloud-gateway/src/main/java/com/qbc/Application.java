21ad10ecb5c15ab497db8538fcd296156778de78
Ma <Ma@192.168.11.7>
2019-01-09 22:24:37 +0900

    Revert "修改网关启动类注解"
    This reverts commit 0a1ebfc3a856c1ba6b11f0fdc121352831f2bb82.

--- qbc-cloud/qbc-cloud-gateway/src/main/java/com/qbc/Application.java
+++ qbc-cloud/qbc-cloud-gateway/src/main/java/com/qbc/Application.java
@@ -1,9 +1,11 @@
 package com.qbc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringCloudApplication
+@SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {

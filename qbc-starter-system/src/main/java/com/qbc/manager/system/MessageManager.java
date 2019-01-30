91bdd74d68bd44367c25e78b5ff0de697b89c230
Ma <Ma@192.168.11.7>
2019-01-30 11:16:05 +0900

    国际化

--- qbc-starter-system/src/main/java/com/qbc/manager/system/MessageManager.java
+++ qbc-starter-system/src/main/java/com/qbc/manager/system/MessageManager.java
@@ -8,10 +8,12 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.support.AbstractMessageSource;
 import org.springframework.stereotype.Component;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.qbc.dao.system.SystemMessageDAO;
 import com.qbc.dao.system.SystemMessageDO;
 
 @Component
+@DS("system")
 public class MessageManager extends AbstractMessageSource {
 
 	@Autowired
